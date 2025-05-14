9c98d7a90b84f0534386aa3d76058691ba6068b5
pbohea <pbohea@gmail.com>
2025-05-14 01:32:01 +0000

    made sign out work for user

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,26 +1,25 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-
-  protected
-
-  def after_sign_in_path_for(resource)
-    user_dashboard_path(resource) 
-  end
-
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
-  end
-
   def create
     super do |user|
-      track_user_session(user) 
+      track_user_session(user)
     end
   end
 
   def destroy
     cookies.delete(:user_id)
     super
+  end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    user_dashboard_path(resource)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
   end
 
   private
@@ -31,27 +30,24 @@ class Users::SessionsController < Devise::SessionsController
   end
 end
 
+# GET /resource/sign_in
+# def new
+#   super
+# end
 
+# POST /resource/sign_in
+# def create
+#   super
+# end
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+# DELETE /resource/sign_out
+# def destroy
+#   super
+# end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+# protected
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
-
+# If you have extra params to permit, append them to the sanitizer.
+# def configure_sign_in_params
+#   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+# end
