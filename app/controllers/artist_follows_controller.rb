d5ebed8fffd4edccc2403b77f28d4f7429a46a95
pbohea <pbohea@gmail.com>
2025-05-16 16:03:04 +0000

    got follow and unfollow turbo working

--- app/controllers/artist_follows_controller.rb
+++ app/controllers/artist_follows_controller.rb
@@ -1,24 +1,58 @@
+# class ArtistFollowsController < ApplicationController
+
+# before_action :authenticate_user!
+
+# def create
+#   @artist = Artist.find(params[:artist_id])
+#   @follow = current_user.artist_follows.create(artist: @artist)
+
+#   respond_to do |format|
+#     format.turbo_stream
+#     format.html { redirect_to artist_path(@artist) }
+#   end
+# end
+
+# def destroy
+#   @follow = current_user.artist_follows.find(params[:id])
+#   @artist = @follow.artist
+#   @follow.destroy
+
+#   respond_to do |format|
+#     format.turbo_stream
+#     format.html { redirect_to artist_path(@artist) }
+#   end
+# end
+
 class ArtistFollowsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :set_artist_for_create, only: [:create]
+  before_action :set_artist_for_destroy, only: [:destroy]
 
   def create
-    @artist = Artist.find(params[:artist_id])
-    @follow = current_user.artist_follows.create(artist: @artist)
+    current_user.artist_follows.create!(artist: @artist)
 
     respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to artist_path(@artist) }
+      format.turbo_stream  # renders create.turbo_stream.erb
+      format.html { redirect_to @artist }
     end
   end
 
   def destroy
-    @follow = current_user.artist_follows.find(params[:id])
-    @artist = @follow.artist
-    @follow.destroy
+    current_user.artist_follows.find_by(artist: @artist)&.destroy
 
     respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to artist_path(@artist) }
+      format.turbo_stream  # renders destroy.turbo_stream.erb
+      format.html { redirect_to @artist }
     end
+  end
+
+  private
+
+  def set_artist_for_create
+    @artist = Artist.find(params[:artist_id])
+  end
+
+  def set_artist_for_destroy
+    follow = ArtistFollow.find(params[:id])
+    @artist = follow.artist
   end
 end
