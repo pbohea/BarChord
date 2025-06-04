29862606064b182ecb942d6baa907a1b98bf742b
pbohea <pbohea@gmail.com>
2025-06-04 21:54:23 +0000

    sign up buttons better

--- app/javascript/controllers/auth_prompt_controller.js
+++ app/javascript/controllers/auth_prompt_controller.js
@@ -1,10 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = { message: String }
+
   prompt(event) {
     event.preventDefault()
     
-    if (confirm("You need to sign in to follow artists. Go to sign in page?")) {
+    // Use custom message if provided, otherwise use default
+    const message = this.messageValue || "You need to sign in. Go to sign in page?"
+    
+    if (confirm(message)) {
       window.location.href = "/users/sign_in"
     }
   }
