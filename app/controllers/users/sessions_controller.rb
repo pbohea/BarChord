dbb91c20165353c0b73067df50d84e789738cfc0
pbohea <pbohea@gmail.com>
2025-06-06 16:21:56 +0000

    progress on user up/in/out

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -19,7 +19,7 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    root_path
+    menu_path
   end
 
   def respond_to_on_destroy
@@ -34,4 +34,3 @@ class Users::SessionsController < Devise::SessionsController
     Current.user = user if defined?(Current)
   end
 end
-
