43a7a6cd46f60e915127ef788e349957e27f8cae
pbohea <pbohea@gmail.com>
2025-06-18 18:20:57 +0000

    trying server stuff

--- app/notifiers/new_event_notifier.rb
+++ app/notifiers/new_event_notifier.rb
@@ -1,50 +1,29 @@
-# class NewEventNotifier < ApplicationNotifier
-#   required_param :event
-#   deliver_by :ios do |config|
-#     config.device_tokens = -> {
-#       recipient.notification_tokens.where(platform: :ios).pluck(:token)
-#     }
-#     config.format = ->(apn) {
-#       apn.alert = "New event from one of your favorite artists"
-#       apn.custom_payload = {
-#         path: event_path(params[:event])
-#       }
-#     }
-#     credentials = Rails.application.credentials.ios
-#     config.bundle_identifier = credentials.bundle_identifier
-#     config.key_id = credentials.key_id
-#     config.team_id = credentials.team_id
-#     config.apns_key = credentials.apns_key
-#     config.delay = false
-
-#     config.development = Rails.env.local?
-#   end
-# end
-
 class NewEventNotifier < ApplicationNotifier
   required_param :event
+
   deliver_by :ios do |config|
     config.device_tokens = -> {
-      # Use the polymorphic association explicitly
       NotificationToken.where(
-        notifiable_type: 'User', 
-        notifiable_id: recipient.id, 
+        notifiable_type: 'User',
+        notifiable_id: recipient.id,
         platform: 'iOS'
       ).pluck(:token)
     }
+
     config.format = ->(apn) {
       apn.alert = "New event from one of your favorite artists"
       apn.custom_payload = {
         path: event_path(params[:event])
       }
     }
-    credentials = Rails.application.credentials.ios
-    config.bundle_identifier = credentials.bundle_identifier
-    config.key_id = credentials.key_id
-    config.team_id = credentials.team_id
-    config.apns_key = credentials.apns_key
-    config.delay = false
 
-    config.development = false  # TestFlight needs production APNS
+    # 🛠️ Wrap all credential access in lambdas so they're fetched at runtime
+    config.bundle_identifier = -> { Rails.application.credentials.dig(:ios, :bundle_identifier) }
+    config.key_id            = -> { Rails.application.credentials.dig(:ios, :key_id) }
+    config.team_id           = -> { Rails.application.credentials.dig(:ios, :team_id) }
+    config.apns_key          = -> { Rails.application.credentials.dig(:ios, :apns_key) }
+
+    config.delay = false
+    config.development = false  # Production push only (for TestFlight)
   end
 end
