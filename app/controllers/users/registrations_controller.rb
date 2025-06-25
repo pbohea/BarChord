360c68505f7c340bb6f7aba33830859bbd20da8a
pbohea <pbohea@gmail.com>
2025-06-25 16:14:25 +0000

    last try

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,56 @@
+# class Users::RegistrationsController < Devise::RegistrationsController
+#   # GET /users/sign_up
+#   def new
+#     super
+#   end
+
+#   # POST /users
+#   def create
+#     super do |user|
+#       track_user_session(user)
+#     end
+#   end
+
+#   # PUT /users
+#   def update
+#     super
+#   end
+
+#   protected
+
+#   # def after_sign_up_path_for(resource)
+#   #   stored_location_for(resource) || user_dashboard_path(resource)
+#   # end
+
+#   def after_sign_up_path_for(resource)
+#     @stored_path ||= stored_location_for(resource)
+#     Rails.logger.info "=== STORED LOCATION: #{@stored_path.inspect} ==="
+#     Rails.logger.info "=== USING PATH: #{@stored_path || user_dashboard_path(resource)} ==="
+
+#     @stored_path || user_dashboard_path(resource)
+#   end
+
+#   def after_update_path_for(resource)
+#     stored_location_for(resource) || user_dashboard_path(resource)
+#   end
+
+#   def respond_with(resource, _opts = {})
+#     if resource.persisted?
+#       # Force a proper HTML redirect instead of turbo_stream
+#       redirect_to after_sign_up_path_for(resource), status: :see_other
+#     else
+#       super
+#     end
+#   end
+
+#   private
+
+#   def track_user_session(user)
+#     cookies.permanent.encrypted[:user_id] = user.id
+#     Current.user = user if defined?(Current)
+#   end
+# end
+
 class Users::RegistrationsController < Devise::RegistrationsController
   # GET /users/sign_up
   def new
@@ -6,9 +59,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /users
   def create
-    super do |user|
-      track_user_session(user)
-    end
+    super
   end
 
   # PUT /users
@@ -17,10 +68,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   protected
-
-  # def after_sign_up_path_for(resource)
-  #   stored_location_for(resource) || user_dashboard_path(resource)
-  # end
 
   def after_sign_up_path_for(resource)
     @stored_path ||= stored_location_for(resource)
@@ -32,21 +79,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def after_update_path_for(resource)
     stored_location_for(resource) || user_dashboard_path(resource)
-  end
-
-  def respond_with(resource, _opts = {})
-    if resource.persisted?
-      # Force a proper HTML redirect instead of turbo_stream
-      redirect_to after_sign_up_path_for(resource), status: :see_other
-    else
-      super
-    end
-  end
-
-  private
-
-  def track_user_session(user)
-    cookies.permanent.encrypted[:user_id] = user.id
-    Current.user = user if defined?(Current)
   end
 end
