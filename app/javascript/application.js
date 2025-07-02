b97e51b5982a6773cdd61de755877975f0e1164f
pbohea <pbohea@gmail.com>
2025-07-02 20:04:17 +0000

    removed bootstrap import that caused issues

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "bootstrap"
 
 // Change to true to allow Turbo
 Turbo.session.drive = true
