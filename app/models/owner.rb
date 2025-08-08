512535cd17672e4d9650c7cdb2110b2bc9397b62
pbohea <pbohea@gmail.com>
2025-08-08 20:35:11 +0000

    removed username from owner

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -10,7 +10,6 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
-#  username               :string
 #  venuescount            :integer
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_07_154944) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_08_203244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -143,7 +143,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_07_154944) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "username"
     t.string "firstname"
     t.string "lastname"
     t.integer "venuescount"
